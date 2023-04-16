711114e4cdb84406fa426e3136420de79c4865ce
GitOfVysakh <vysakh@steyp.com>
2023-04-16 12:52:55 +0530

    created first page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn to travel <a href="/screens/home">home</a>
         </h1>
 
         <p className={styles.description}>
